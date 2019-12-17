a39d2ce0d34e3aaa90ed5c4bccaa6d4addf10c0c
z3to <6gqrbj@posteo.org>
2019-12-17 17:12:06 +0100

    Ressources styling

--- components/GalleryModule.vue
+++ components/GalleryModule.vue
@@ -1,39 +1,56 @@
 <template>
   <div class="gallery-module">
-    <header :class="{ 'module-header': true }" :style="{ 'background-image': `url(./bg/${bg}.png)`}">
+    <header :class="{ 'module-header': true, isExpanded }" :style="{ 'background-image': `url(./bg/${bg}.png)`}">
       <h2 class="module-title mono">
         {{ title }}
       </h2>
-      <p class="module-description">
-        {{ description }}
-      </p>
+      <transition-expand>
+        <p v-if="!isExpanded" class="module-description">
+          {{ description }}
+        </p>
+      </transition-expand>
     </header>
     <footer class="module-footer">
       <a v-if="path.length" :href="`/${path}`" class="btn btn-module">Read the module</a>
-      <ul>
-        <li>By {{ author }}</li>
-        <li>
-          <span
-            v-for="tag in tags"
-            :key="tag"
-            :class="{ tag: true, [tag]: true }"
-            @click="() => changeFilter({ key: 'tag', value: tag })">{{ tag }}</span>
-        </li>
-        <li v-if="gem">
-          <span class="btn btn--tertiary">Explore the data</span>
-        </li>
-        <li v-if="share">
-          <span class="btn btn--tertiary">Download ressources</span>
-        </li>
-      </ul>
-      <!-- <button class="btn btn--secondary" v-if="gem">Explore the data</button>
-      <button class="btn btn--secondary" v-if="share">Download ressources</button> -->
+      <span @click="expand" class="btn btn-expand" :class="{ isExpanded }">More information <svg width="16" height="8"><g><path d="M-4,1.5 L0,-1.5 L4,1.5"/></g></svg></span>
+      <transition-expand>
+        <ul v-if="isExpanded" class="items">
+          <li>
+            <span class="caption">Authors</span>
+            <span>By {{ author }}</span>
+          </li>
+          <li>
+            <span class="caption">Audience</span>
+            <span
+              v-for="tag in tags"
+              :key="tag"
+              :class="{ tag: true, [tag]: true }"
+              @click="() => changeFilter({ key: 'tag', value: tag })"
+            >{{ tag }}</span>
+          </li>
+          <li v-if="gem" class="wide">
+            <span class="caption">Gems for this module</span>
+            <ul class="list">
+              <li>
+                <a href="https://data.ene.iiasa.ac.at/iamc-1.5c-explorer//#/workspaces/share/1a6e44c8-0963-4f28-a871-2b81bfb11def">PEP Emissions by major economies (iiasa.ac.at) <i>&nearr;</i></a>
+              </li>
+            </ul>
+          </li>
+          <li v-if="share" class="wide">
+            <span class="caption">Download ressources</span>
+            <ul class="list">
+              <a href="#">Static visualisation graphics</a>
+            </ul>
+          </li>
+        </ul>
+      </transition-expand>
     </footer>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import TransitionExpand from '~/components/TransitionExpand.vue'
 import { chain } from '~/assets/js/utils.js'
 
 export default {
@@ -75,6 +92,11 @@ export default {
       default: () => []
     }
   },
+  data () {
+    return {
+      isExpanded: false
+    }
+  },
   computed: {
     author () {
       return chain(this.authors)
@@ -83,7 +105,13 @@ export default {
   methods: {
     ...mapActions([
       'changeFilter'
-    ])
+    ]),
+    expand () {
+      this.isExpanded = !this.isExpanded
+    }
+  },
+  components: {
+    TransitionExpand
   }
 }
 </script>
@@ -98,7 +126,6 @@ export default {
   $padding-vertical: $spacing / 3 * 2;
 
   .gallery-module {
-    box-shadow: $box-shadow--default;
     background-color: #fff;
     display: grid;
     grid-row: span 2;
@@ -107,6 +134,8 @@ export default {
     grid-row-gap: 0;
     width: 100%;
     margin-bottom: $spacing;
+    min-height: 350px;
+    box-shadow: $box-shadow--default;
 
     @include media-query($medium) {
       margin-bottom: 0;
@@ -120,7 +149,7 @@ export default {
       }
 
       // padding-bottom: $spacing * 2;
-      min-height: 250px;
+      // min-height: 250px;
       border-bottom: 1px solid getColor(gray, 80);
       background-position: center;
       background-size: 90% auto;
@@ -135,13 +164,18 @@ export default {
       &::after {
         content: "";
         background-color: #fff;
-        opacity: 0.7;
+        opacity: 1;
         top: 0;
         left: 0;
         bottom: 0;
         right: 0;
         position: absolute;
         z-index: 1;
+        transition: opacity 1s;
+      }
+
+      &.isExpanded::after {
+        opacity: 1;
       }
 
       &.alignRight {
@@ -149,12 +183,12 @@ export default {
       }
 
       .module-title {
-        margin-bottom: $spacing / 2;
         display: inline-block;
         z-index: 2;
       }
 
       .module-description {
+        margin-top: $spacing / 2;
         max-width: $text-width;
         z-index: 2;
       }
@@ -167,7 +201,8 @@ export default {
       // grid-row-gap: $spacing / 4;
       padding: 0 $padding-horizontal $padding-vertical;
       align-items: start;
-      box-shadow: inset 0 20px 20px -20px rgba(getColor(gray, 80), 0.8);
+      box-shadow: inset 0 20px 20px -20px rgba(getColor(gray, 80), 0.5);
+      position: relative;
 
       .btn-module {
         place-self: flex-end;
@@ -178,7 +213,7 @@ export default {
         border-radius: 50%;
         padding: 0;
         box-shadow: $box-shadow--strong;
-        transition: transform 0.3s, background-color 0.3s, color 0.3s;
+        transition: transform $transition, background-color $transition, color $transition;
 
         &:hover {
           transform: scale(1.1);
@@ -188,7 +223,53 @@ export default {
         }
       }
 
-      ul {
+      .btn-expand {
+        place-self: flex-start;
+        position: absolute;
+        border-radius: $spacing / 4;
+        padding: $spacing / 8 $spacing / 2;
+        height: $spacing;
+        left: $padding-horizontal;
+        top: -$spacing / 2;
+        z-index: 2;
+        transition: background-color $transition, color $transition;
+        background-color: #fff;
+        border: 1px solid getColor(gray, 80);
+        color: $color-deep-gray;
+
+        svg {
+          g {
+            transform: translate(11px, 4px);
+            path {
+              stroke: $color-deep-gray;
+              stroke-width: 1.5;
+              fill: none;
+              transition: transform $transition, stroke $transition;
+              transform: rotate(0deg);
+            }
+          }
+        }
+
+        &:hover {
+          color: $color-neon;
+
+          svg g path {
+            stroke: $color-neon;
+          }
+        }
+
+        &.isExpanded {
+          svg {
+            g {
+              path {
+                transform: rotate(180deg);
+              }
+            }
+          }
+        }
+      }
+
+      ul.items {
         list-style: none;
         display: grid;
         grid-template-columns: repeat(2, 1fr);
@@ -199,8 +280,17 @@ export default {
         }
 
         @include media-query($medium) {
-          grid-gap: $spacing $spacing / 2;
+          grid-gap: $spacing / 2 $spacing / 2;
         }
+
+        .wide {
+          grid-column-end: span 2;
+        }
+      }
+
+      ul.list {
+        list-style: none;
+        font-size: 0.8rem;
       }
     }
   }
