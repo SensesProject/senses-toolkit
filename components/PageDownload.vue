d53b0f2a20cd4169d316d22197f3550cec38b482
z3to <6gqrbj@posteo.org>
2019-12-19 21:16:25 +0100

    Download styling

--- components/PageDownload.vue
+++ components/PageDownload.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="page-download">
-    <h1>{{ item.label }}</h1>
+    <header>
+      <h1>{{ item.label }}</h1>
+    </header>
+    <dl class="details">
+      <dt class="caption">Authors</dt>
+      <dd>{{ author }}</dd>
+      <dt class="caption">Licence</dt>
+      <dd>{{ item.licence }}</dd>
+      <dt class="caption">Reference</dt>
+      <dd>
+        <div class="readonly">
+          <input type="text" readonly :value="item.reference" />
+          <button class="btn">Copy</button>
+        </div>
+      </dd>
+      <dt class="caption">Licence</dt>
+      <dd>{{ item.licence }}</dd>
+    </dl>
+    <footer>
+      <a :href="item.link" class="btn">Download</a>
+    </footer>
+    <span @click="hide" class="close">&times;</span>
     {{ activeID }}
     {{ item }}
   </div>
@@ -9,6 +30,7 @@
 <script>
 import { mapState, mapGetters } from 'vuex'
 import { get } from 'lodash'
+import { chain } from '~/assets/js/utils.js'
 
 export default {
   computed: {
@@ -20,6 +42,14 @@ export default {
     ]),
     item () {
       return get(this.downloads, this.activeID)
+    },
+    author () {
+      return chain(this.item.authors)
+    }
+  },
+  methods: {
+    hide () {
+      this.$modal.hide('download')
     }
   }
 }
@@ -27,5 +57,54 @@ export default {
 
 <style lang="scss" scoped>
 @import "~@/assets/style/global";
+
+.page-download {
+  margin: $spacing;
+  display: grid;
+  grid-row-gap: $spacing;
+  position: relative;
+
+  .close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    font-size: 1.5rem;
+    line-height: 1;
+    color: $color-neon;
+    cursor: pointer;
+
+    &:hover {
+      color: #000;
+    }
+  }
+
+  .details {
+    dd {
+      margin-bottom: $spacing / 2;
+    }
+  }
+
+  .readonly {
+    border-radius: $border-radius;
+    margin-right: 8px;
+    border: 1px solid rgba($color-gray, 0.2);
+    display: grid;
+    grid-template-columns: 1fr auto;
+
+    input {
+      border: 0;
+      padding: $spacing / 4;
+      font-size: 0.8rem;
+
+      &:focus {
+        outline: none;
+      }
+    }
+
+    button {
+      padding: $spacing / 4 $spacing;
+    }
+  }
+}
 
 </style>
