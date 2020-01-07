0ed200552939ad87da033f3bae1ddea2e49d4727
z3to <6gqrbj@posteo.org>
2020-01-07 14:36:52 +0100

    Add tooltip functionality

--- store/index.js
+++ store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VModal from 'vue-js-modal'
+import VTooltip from 'v-tooltip'
 import data from './modules/data'
 import filter from './modules/filter'
 import options from './modules/options'
@@ -8,6 +9,7 @@ import downloads from './modules/downloads'
 
 Vue.use(Vuex)
 Vue.use(VModal)
+Vue.use(VTooltip)
 
 export const modules = {
   data,
