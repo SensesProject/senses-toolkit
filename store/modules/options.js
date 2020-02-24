60d6f8047df08cfec0fef431507c0b94c846b527
z3to <6gqrbj@posteo.org>
2020-02-24 17:43:37 +0100

    Add more filter options

--- store/modules/options.js
+++ store/modules/options.js
@@ -3,7 +3,10 @@ import { uniq, flatten, map, get } from 'lodash'
 const getters = {
   tags: (state, getters, rootState) => {
     const options = [{ 'label': 'Show all', 'value': false }]
+    options.push({ text: 'Filter by audience' })
     options.push(...uniq(flatten(map(get(rootState, ['data', 'datum', 'data'], []), 'tags'))))
+    options.push({ text: 'Filter by type' })
+    options.push(...uniq(flatten(map(get(rootState, ['data', 'datum', 'data'], []), 'type'))))
     return options
   }
 }
