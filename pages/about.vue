af3408c2f192f4ba346249665f9cb9a139ecc87d
z3to <6gqrbj@posteo.org>
2020-04-05 22:18:15 +0200

    new about text

--- pages/about.vue
+++ pages/about.vue
@@ -2,14 +2,18 @@
   <div class="container">
     <PageHeader />
     <PageContainer>
-      <h1>How to use the toolkit</h1>
+      <h1>How to use the SENSES Tookit</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        The SENSES Toolkit is a unique collection of user-centred scenario visualization tools, include practical guidelines and manuals, and builds on co-production techniques of relevant and useful climate change knowledge for three key user groups.
+      </p>
+      <p>
+        The aim of SENSES is to generate tools for debate and deliberation. The interactive approach of co-production prevents that users become detached from the original scenario data, and minimizes the risk that the extracted information is misinterpreted and/or perceived as intransparent. Thus, an essential objective of SENSES is that the developed tools are understandable, accessible, trustworthy, and useful for the stakeholders.
+      </p>
+      <p>
+        The following video is a quick tutorial on the key features of the SENSES Toolkit:
+      </p>
+      <p>
+        <img :src="`${$router.options.base}/SENSES-Placeholder.jpg`" alt="Video Placeholder">
       </p>
     </PageContainer>
     <PageFooter />
@@ -37,6 +41,12 @@ export default {
     display: flex;
     align-items: center;
     flex-direction: column;
+
+    img {
+      width: 100%;
+      border: 1px solid #eee;
+      margin-top: $spacing;
+    }
   }
 
 </style>
